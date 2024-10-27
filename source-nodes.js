ea690fefd8ed12986561c7d7d9e4d5932795ef6e
Duane Cilliers <hello@duane.co.za>
2024-10-27 12:21:06 +0200

    fix: max results 500, add pagination

--- source-nodes.js
+++ source-nodes.js
@@ -31,54 +31,69 @@ const getNodeData = (gatsby, media) => {
   }
 }
 
-module.exports = function sourceNodes (gatsby, options) {
+module.exports = async function sourceNodes (gatsby, options) {
   const cloudinary = newCloudinary(options)
   const resourceOptions = getResourceOptions(options)
 
   const { cloudName } = options
 
-  return cloudinary.api.resources(resourceOptions, (error, result) => {
-    const hasResources = (result && result.resources && result.resources.length)
+  const { max_results, results_per_page } = resourceOptions;
 
-    if (error) {
+  let nextCursor = null;
+  let limit = max_results;
+  let resultsPerPage = results_per_page ? results_per_page : 500;
+
+  do {
+    try {
+      const result = await cloudinary.api.resources({
+        ...resourceOptions,
+        max_results: limit < resultsPerPage ? limit : resultsPerPage,
+        next_cursor: nextCursor
+      })
+
+      const hasResources = (result && result.resources && result.resources.length)
+
+      if (!hasResources) {
+        console.warn('\n ~Yikes! No nodes created because no Cloudinary resources found. Try a different query?')
+        return
+      }
+
+      result.resources.forEach(async (resource) => {
+
+        const nodeData = getNodeData(gatsby, resource)
+        await gatsby.actions.createNode(nodeData)
+
+        const { id, public_id, version, height, width } = nodeData
+        const node = gatsby.getNode(id)
+
+        if (nodeData.resource_type === 'image') {
+          const imageNodeData = {
+            id: gatsby.createNodeId(`cloudinary-media-image-${id}`),
+            public_id,
+            cloudName,
+            version,
+            parent: id,
+            originalHeight: height,
+            originalWidth: width,
+            breakpoints: [320, 640, 1280, 2560],
+            internal: {
+              type: imageType,
+              contentDigest: gatsby.createContentDigest(gatsby.getNode(id))
+            }
+          }
+
+          await gatsby.actions.createNode(imageNodeData)
+          gatsby.actions.createParentChildLink({ parent: node, child: imageNodeData })
+        }
+      })
+
+      console.info(`Added ${hasResources} CloudinaryMedia ${hasResources > 1 ? 'nodes' : 'node'}`)
+
+      nextCursor = result.next_cursor;
+      limit = limit - result.resources.length;
+    } catch (error) {
       console.error(error)
       return
     }
-
-    if (!hasResources) {
-      console.warn('\n ~Yikes! No nodes created because no Cloudinary resources found. Try a different query?')
-      return
-    }
-
-    result.resources.forEach(async (resource) => {
-
-      const nodeData = getNodeData(gatsby, resource)
-      await gatsby.actions.createNode(nodeData)
-
-      const { id, public_id, version, height, width } = nodeData
-      const node = gatsby.getNode(id)
-
-      if (nodeData.resource_type === 'image') {
-        const imageNodeData = {
-          id: gatsby.createNodeId(`cloudinary-media-image-${id}`),
-          public_id,
-          cloudName,
-          version,
-          parent: id,
-          originalHeight: height,
-          originalWidth: width,
-          breakpoints: [320, 640, 1280, 2560],
-          internal: {
-            type: imageType,
-            contentDigest: gatsby.createContentDigest(gatsby.getNode(id))
-          }
-        }
-
-        await gatsby.actions.createNode(imageNodeData)
-        gatsby.actions.createParentChildLink({ parent: node, child: imageNodeData })
-      }
-    })
-
-    console.info(`Added ${hasResources} CloudinaryMedia ${hasResources > 1 ? 'nodes' : 'node'}`)
-  })
+  } while (nextCursor && limit > 0);
 }
